f7d7d324d82f882c18ef7a17643830e556080195
Alexandra Decker <alidecker@Jennifer-Deckers-MacBook-Pro.local>
2018-02-04 16:42:33 -0500

    Made filtering work

--- filtering.js
+++ filtering.js
@@ -3,10 +3,11 @@ function filterSelection(c) {
   var x, i;
   x = document.getElementsByClassName("project");
   if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
     w3RemoveClass(x[i], "show");
+    w3RemoveClass(x[i], "noshow");
     if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    if (x[i].className.indexOf(c) == -1) w3AddClass(x[i], "noshow");
   }
 }
 // Show filtered elements
