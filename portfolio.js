9451d09d41b4247537f5a6a8a422e226e266b60b
Alexandra Decker <alidecker@Jennifer-Deckers-MacBook-Pro.local>
2018-02-04 16:26:10 -0500

    Added filter buttons

---
+++ portfolio.js
@@ -0,0 +1,67 @@
+// Show/hide project details when clicked
+function thisWebFunc() {
+    var web = document.getElementById("show-thisWeb");
+    var w_bg = document.getElementById("all-thisWeb");
+    if (web.style.display === "none") {
+        web.style.display = "block";
+        w_bg.style.background = "#f7eede";
+    } else {
+        web.style.display = "none";
+        w_bg.style.background = "#F5DEB3";
+    }
+}
+function sonosFunc() {
+    var sonos = document.getElementById("show-sonos");
+    var s_bg = document.getElementById("all-sonos");
+    if (sonos.style.display === "none") {
+        sonos.style.display = "block";
+        s_bg.style.background = "#f7eede";
+    } else {
+        sonos.style.display = "none";
+        s_bg.style.background = "#F5DEB3";
+    }
+}
+function justoCatFunc() {
+    var justoCat = document.getElementById("show-justocat");
+    var j_bg = document.getElementById("all-justocat")
+    if (justoCat.style.display === "none") {
+        justoCat.style.display = "block";
+        j_bg.style.background = "#f7eede";
+    } else {
+        justoCat.style.display = "none";
+        j_bg.style.background = "#F5DEB3";
+    }
+}
+function ehangFunc() {
+    var ehang = document.getElementById("show-ehang");
+    var e_bg = document.getElementById("all-ehang")
+    if (ehang.style.display === "none") {
+        ehang.style.display = "block";
+        e_bg.style.background = "#f7eede";
+    } else {
+        ehang.style.display = "none";
+        e_bg.style.background = "#F5DEB3";
+    }
+}
+function trainFunc() {
+    var train = document.getElementById("show-train");
+    var t_bg = document.getElementById("all-train");
+    if (train.style.display === "none") {
+        train.style.display = "block";
+        t_bg.style.background = "#f7eede";
+    } else {
+        train.style.display = "none";
+        t_bg.style.background = "#F5DEB3";
+    }
+}
+function artFunc() {
+    var art = document.getElementById("show-art");
+    var a_bg = document.getElementById("all-art");
+    if (art.style.display === "none") {
+        art.style.display = "block";
+        a_bg.style.background = "#f7eede";
+    } else {
+        art.style.display = "none";
+        a_bg.style.background = "#F5DEB3";
+    }
+}
